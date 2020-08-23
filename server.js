7ca6530584615edf316510c767f07e6f450d079c
dmeegan <dn_meegan@yahoo.com>
2020-08-23 14:48:40 -0400

    Fixed the get routes to work properly by formatting as objects returned

--- server.js
+++ server.js
@@ -8,21 +8,18 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
-app.get('api/quotes/random', (req, res, next) => {
-    const quoteReturned = getRandomElement(quotes);
-    if (quoteReturned) {
-        res.status(204).send(quoteReturned)
-    } else {
-        res.status(404).send('Oops! Something went wrong.')
-    }=
+app.get('/api/quotes/random', (req, res, next) => {
+    const quoteToReturn = (getRandomElement(quotes));
+    res.send({quote: quoteToReturn});   
 })
 
-app.get('api/quotes/:person', (req, res, next) => {
-    if (req.params.id){
-        const quotesByPerson = quotes.filter(() => quotes.person === req.params.id);
-        res.send(quotesByPerson);
+app.get('/api/quotes', (req, res, next) => {
+    const person = req.query.person;
+    if (person){
+        const quotesByPerson = quotes.filter((quote) => quote.person === person);
+        res.send({quotes: quotesByPerson});
     } else {
-        res.send(quotes);
+        res.send({quotes: quotes});
     }
 })
 
