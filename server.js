5fc31372ddc5dcc7e40c72eff6c50552e10d637c
dmeegan <dn_meegan@yahoo.com>
2020-08-23 13:32:38 -0400

    Added get route to return a random quote

--- server.js
+++ server.js
@@ -8,5 +8,23 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+app.get('api/quotes/random', (req, res, next) => {
+    const quoteReturned = getRandomElement(quotes);
+    if (quoteReturned) {
+        res.status(204).send(quoteReturned)
+    } else {
+        res.status(404).send('Oops! Something went wrong.')
+    }=
+})
+
+app.get('api/quotes/:person', (req, res, next) => {
+    if (req.params.id){
+        const quotesByPerson = quotes.filter(() => quotes.person === req.params.id);
+        res.send(quotesByPerson);
+    } else {
+        res.send(quotes);
+    }
+})
+
 app.listen(PORT);
 
