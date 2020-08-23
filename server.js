83b7a76c6a9c4838b9ce65b6be2de63bc788a2b4
dmeegan <dn_meegan@yahoo.com>
2020-08-23 15:45:32 -0400

    Added a post request for adding a quote to the quotes list based on the user input for the text and person

--- server.js
+++ server.js
@@ -8,11 +8,15 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+
+//GET request for returning a random quote from the quotes list.
 app.get('/api/quotes/random', (req, res, next) => {
     const quoteToReturn = (getRandomElement(quotes));
     res.send({quote: quoteToReturn});   
-})
+});
 
+
+//GET request for returning a list of quotes attributed to a particular person, based on the user input for the person.
 app.get('/api/quotes', (req, res, next) => {
     const person = req.query.person;
     if (person){
@@ -20,6 +24,19 @@ app.get('/api/quotes', (req, res, next) => {
         res.send({quotes: quotesByPerson});
     } else {
         res.send({quotes: quotes});
+    }
+});
+
+//POST request for adding a quote to the quotes list based on the user input for the quote and the person who it is attributed to.
+app.post('/api/quotes', (req, res, next) => {
+    const quoteToAdd = req.query.quote;
+    const personToAdd = req.query.person;
+    if (quoteToAdd && personToAdd) {
+        const newQuote = {quote: quoteToAdd, person: personToAdd};
+        quotes.push(newQuote);
+        res.send(newQuote)
+    } else {
+        res.status(400).send('Please enter a quote and the attributing person.');
     }
 })
 
